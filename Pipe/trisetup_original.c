8a7b669e8706fae67d8e115b8b120482cb7fc485
Landon Arnold <36831950+AX-I@users.noreply.github.com>
2020-01-18 21:39:34 -0500

    Replace int with float screen coordinates

--- Pipe/trisetup_original.c
+++ Pipe/trisetup_original.c
@@ -4,17 +4,16 @@ __kernel void setup(__global float3 *XYZ,
                     __global float3 *VN,
                     __global int *TI,  // Indices out
                     __global int *TN,  // number of tris
-                    __global int2 *XY,   // Screen coords out
+                    __global float2 *XY,   // Screen coords out
                     __global float *ZZ,  // Depths out
                     __constant float *Vpos, __constant float3 *VV,
                     const float sScale, const int wF, const int hF,
                     const float cAX, const float cAY, const int lenP) {
-    
+
     // Block index
     int bx = get_group_id(0);
     int tx = get_local_id(0);
-    
-    //if ((bx == 0) && (tx == 0)) printf("Trisetup \n");
+
     if (tx == 0) TN[bx] = 0;
     barrier(CLK_GLOBAL_MEM_FENCE);
 
@@ -23,7 +22,7 @@ __kernel void setup(__global float3 *XYZ,
     float3 x1 = XYZ[ci];
     float3 x2 = XYZ[ci+1];
     float3 x3 = XYZ[ci+2];
-    
+
     float3 vp = (float3)(Vpos[0], Vpos[1], Vpos[2]);
     x1 -= vp; x2 -= vp; x3 -= vp;
     float3 SVd = VV[0];
@@ -38,15 +37,19 @@ __kernel void setup(__global float3 *XYZ,
     if (all(dd > 0) && all(fabs(dx) < cAX) && all(fabs(dy) < cAY)) {
       yes = true;
     }
+
+    dx = dx * -sScale + wF/2;
+    dy = dy * sScale + hF/2;
+    int3 dix = convert_int3(dx);
+    int3 diy = convert_int3(dy);
+    if (max(dix.x, max(dix.y, dix.z)) - min(dix.x, min(dix.y, dix.z)) == 0) yes = false;
+    if (max(diy.x, max(diy.y, diy.z)) - min(diy.x, min(diy.y, diy.z)) == 0) yes = false;
+
     if (yes) {
-      dx = dx * -sScale + wF/2;
-      dy = dy * sScale + hF/2;
-      int3 dix = convert_int3(dx);
-      int3 diy = convert_int3(dy);
       int nextI = atomic_inc(&TN[bx]);
-      XY[(bx*BLOCK_SIZE + tx)*3] = (int2)(dix.s0, diy.s0);
-      XY[(bx*BLOCK_SIZE + tx)*3+1] = (int2)(dix.s1, diy.s1);
-      XY[(bx*BLOCK_SIZE + tx)*3+2] = (int2)(dix.s2, diy.s2);
+      XY[(bx*BLOCK_SIZE + tx)*3] = (float2)(dx.s0, dy.s0);
+      XY[(bx*BLOCK_SIZE + tx)*3+1] = (float2)(dx.s1, dy.s1);
+      XY[(bx*BLOCK_SIZE + tx)*3+2] = (float2)(dx.s2, dy.s2);
       ZZ[(bx*BLOCK_SIZE + tx)*3] = dd.s0;
       ZZ[(bx*BLOCK_SIZE + tx)*3+1] = dd.s1;
       ZZ[(bx*BLOCK_SIZE + tx)*3+2] = dd.s2;
